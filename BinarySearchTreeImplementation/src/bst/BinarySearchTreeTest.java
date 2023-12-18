3964d3b113274cca639373f1e2ef95a20459d3ea
Jesus_hna <thehacha167@gmail.com>
2023-12-18 20:13:37 +0100

    Delete unnecessary files and refactor BinarySearchTreeTest.java

--- BinarySearchTreeImplementation/src/bst/BinarySearchTreeTest.java
+++ BinarySearchTreeImplementation/src/bst/BinarySearchTreeTest.java
@@ -8,22 +8,21 @@ public class BinarySearchTreeTest {
         BinarySearchTree bst = new BinarySearchTree();
 
         // Pruebas de inserción
-        bst.insert(50);
+        
         bst.insert(30);
-        bst.insert(20);
-        bst.insert(40);
-        bst.insert(70);
-        bst.insert(60);
-        bst.insert(80);
-        bst.insert(10);
-        bst.insert(25);
         bst.insert(35);
+        bst.insert(40);
+        bst.insert(10);
         bst.insert(45);
+        bst.insert(50);
+        bst.insert(15);
         bst.insert(55);
+        bst.insert(60);
+        bst.insert(20);
         bst.insert(65);
-        bst.insert(75);
-        bst.insert(85);
-
+        bst.insert(25);
+        bst.insert(70);
+        bst.insert(5);
 
         // Pruebas de recorrido
         System.out.println("Inorden: " + bst.inorder());
@@ -31,13 +30,13 @@ public class BinarySearchTreeTest {
         System.out.println("Postorden: " + bst.postorder());
 
         // Pruebas de borrado
+        bst.delete(10);
         bst.delete(20);
         bst.delete(30);
+        bst.delete(40);
         bst.delete(50);
+        bst.delete(60);
         bst.delete(70);
-        bst.delete(85);
-        bst.delete(10);
-        
 
         // Recorrido en inorden después de borrar
         System.out.println("Inorden después de borrar: " + bst.inorder());
